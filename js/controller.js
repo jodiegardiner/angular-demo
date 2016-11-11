f8ec237369537a7a4e29721256b1701ba89acf7e
Jodie Gardiner <jodiegardiner@outlook.com>
2016-11-11 15:00:29 +0000

    final version

---
+++ js/controller.js
@@ -0,0 +1,20 @@
+.controller('RegisterController', function($scope, $location, UserAPIService, store) {
+ 
+        $scope.registrationUser = {};
+        var URL = "https://morning-castle-91468.herokuapp.com/";
+ 
+        $scope.submitForm = function() {
+            if ($scope.registrationForm.$valid) {
+                $scope.registrationUser.username = $scope.user.username;
+                $scope.registrationUser.password = $scope.user.password;
+ 
+                UserAPIService.registerUser(URL + "accounts/register/", $scope.registrationUser).then(function(results) {
+                    $scope.data = results.data;
+                    alert("You have successfully registered to Angular Todo");
+                }).catch(function(err) {
+                    alert("Oops! Something went wrong!");
+                    console.log(err);
+                });
+            }
+        }
+    });
